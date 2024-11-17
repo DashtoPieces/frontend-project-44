c80d60ebe7b056a358924757f8b8b64edef1ba54
DashtoPieces <quilleve@gmail.com>
2024-11-17 16:41:24 +0300

    add game IsEven

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
-import greatings from '../src/cli.js';
+import { greatings } from '../src/cli.js';
+import { isEvenGame } from './brain-even.js';
 
 greatings();
+isEvenGame();
